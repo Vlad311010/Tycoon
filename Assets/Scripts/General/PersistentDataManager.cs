e23e1def76f2ebb45d8c078d82bdf00748225331
Vlad <vladyabl15@gmail.com>
2024-04-07 11:55:38 +0200

    PersistentDataManger refactored to save data in json format

--- Assets/Scripts/General/PersistentDataManager.cs
+++ Assets/Scripts/General/PersistentDataManager.cs
@@ -8,30 +8,25 @@ public class PersistentDataManager : MonoBehaviour
     public static PersistentData GameData { get => gameData == null ? Load() : gameData; set => gameData = value; }
 
     private static PersistentData gameData;
-    
+
+    private static string path = Application.persistentDataPath + "/GameData.json";
+
+
     public static void Save()
     {
-        BinaryFormatter bf = new BinaryFormatter();
-        FileStream file = File.Create(Application.persistentDataPath + "/GameData.dat");
-        bf.Serialize(file, gameData);
-        file.Close();
-        
+        string dataJson = JsonUtility.ToJson(gameData);
+        File.WriteAllText(path, dataJson);
         Debug.Log("Persistent Data Saved");
     }
 
     public static PersistentData Load()
     {
-        if (File.Exists(Application.persistentDataPath + "/GameData.dat"))
+        if (File.Exists(path))
         {
-            BinaryFormatter bf = new BinaryFormatter();
-            FileStream fs = File.Open(Application.persistentDataPath + "/GameData.dat", FileMode.Open);
-            PersistentData data = bf.Deserialize(fs) as PersistentData;
-            fs.Close();
-
-            Debug.Assert(data != null, "Failed to load data");
+            gameData = JsonUtility.FromJson<PersistentData>(File.ReadAllText(path));
+            Debug.Assert(gameData != null, "Failed to load data");
             Debug.Log("Persistent Data Loaded");
-            gameData = data;
-            return data;
+            return gameData;
         }
         else
         {
@@ -41,15 +36,11 @@ public class PersistentDataManager : MonoBehaviour
 
     private static PersistentData CreateInitialData()
     {
-        Debug.Log("Persistent Data Initialized");
-        BinaryFormatter bf = new BinaryFormatter();
-        FileStream file = File.Create(Application.persistentDataPath + "/GameData.dat");
 
-        PersistentData data = new PersistentData();
-        bf.Serialize(file, data);
-        file.Close();
+        gameData = new PersistentData();
+        Save();
 
-        return data;
+        return gameData;
     }
 
 }
